1e54512a3953842d38b8020a7c8d3e7e07fff8d0
Chris Crook <ccrook@linz.govt.nz>
2016-05-20 19:38:17 +1200

    Preparing for release

--- LINZ/Geodetic/LinzDefModelBin.py
+++ LINZ/Geodetic/LinzDefModelBin.py
@@ -1,9 +1,14 @@
+# Imports to support python 3 compatibility
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
 from LINZ.DeformationModel import Model, Time
 from collections import namedtuple
 from datetime import datetime
 import os.path
-import LinzGrid
+import ./LinzGrid
 import struct
 import re
 
